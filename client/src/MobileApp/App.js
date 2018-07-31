ec3878840d8fe77ec9d1d6631801aab06c60e502
Sonny Huang <shengh@princeton.edu>
2018-07-31 10:44:41 +0100

    apolloclient

--- client/src/MobileApp/App.js
+++ client/src/MobileApp/App.js
@@ -13,6 +13,12 @@ import { NativeRouter, Platform, StyleSheet, Text, View, Button } from 'react-na
 import Questionnaire from './components/questionaire/questionaire';
 import Question from './components/question/question';
 import Test from './components/testing/testing'
+import { createStackNavigator } from 'react-navigation';
+import Home from "./components/homepage/Home"
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,30 +27,27 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const Navigation = createStackNavigator(
+  {
+    First: {screen: Home},
+    Second: {screen: Questionnaire},
 
-// const Navigation = createStackNavigator(
-//   {
-//   First: {screen: Home},
-//   Second: {screen: Questionnaire}
-// },
-// {
-//   initialRouteName: 'First',
-// }
-// )
-
-
-export default class App extends Component {
-  render() {
-    const language = "English"
-    return (
-      <View style={styles.container}>
-        <Questionnaire language={language}></Questionnaire>
-        {/* <Text>hi</Text> */}
-        {/* <Test></Test> */}
-      </View>
-    );
+  },
+  {
+    initialRouteName: 'First',
   }
-}
+);
+
+const client = new ApolloClient({
+  link: new HttpLink({uri: `http://10.0.2.2:4000`}),
+cache: new InMemoryCache(),
+});
+
+export default () => (
+  <ApolloProvider client={client}>
+    <Navigation />
+    </ApolloProvider>
+);
 
 const styles = StyleSheet.create({
   container: {
