05fa88fc71c4c98b29c0c5ec82d126fe5fc4452a
Sonny Huang <shengh@princeton.edu>
2018-08-07 10:53:22 +0100

    user

--- server/src/resolvers/Mutation/submitanswer.ts
+++ server/src/resolvers/Mutation/submitanswer.ts
@@ -1,7 +1,7 @@
 import { Context, getUserId } from "../../utils"
 export default {
   submitanswer: async (parent, args, ctx: Context, info) => {
-    // const id = getUserId(ctx)
+    const id = getUserId(ctx)
 
 
 if(args.alternativeId == null)
@@ -9,7 +9,7 @@ if(args.alternativeId == null)
     return await ctx.db.mutation.updateUser(
         {
             data:{answers:{create:{value:args.value,question:{connect:{number:args.questionnumber}}}}},
-            where:{id:"cjkcn2lx20k350b66yz9s4m43"}
+            where:{id:id}
         },
         info
             )
@@ -20,7 +20,7 @@ if(args.alternativeId == null)
     return await ctx.db.mutation.updateUser(
         {
             data:{answers:{create:{value:args.value,question:{connect:{number:args.questionnumber}},alternative:{connect:{id:args.alternativeId}}}}},
-            where:{id:"cjkcn2lx20k350b66yz9s4m43"}
+            where:{id:id}
         },
         info
             )
